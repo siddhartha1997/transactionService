d115a3f7f9d71bb9cb7fc15d7e0a072dc0cd570e
Siddhartha Banerjee <sbanjee.banerjee@gmail.com>
2020-09-29 21:18:33 +0530

    Yaml Added

---
+++ Controllers/TransactionController.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using TransactionMicroservice.Repository;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace TransactionMicroservice.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TransactionController : ControllerBase
+    {
+        static readonly log4net.ILog _log4net = log4net.LogManager.GetLogger(typeof(TransactionController));
+        ITransactionRepository transactionRepository;
+        public TransactionController(ITransactionRepository _transactionRepository)
+        {
+            transactionRepository = _transactionRepository;
+        }
+        [HttpPost]
+        [Route("deposit")]
+        public IActionResult deposit([FromBody]transactionInput value)
+        {
+            try
+            {
+                var ob = transactionRepository.deposit(value);
+                if(ob==null)
+                {
+                    return NotFound();
+                }
+                return Ok(ob);
+            }
+            catch(Exception)
+            {
+                return BadRequest();
+            }
+        }
+        [HttpPost]
+        [Route("withdraw")]
+        public IActionResult withdraw([FromBody] transactionInput value)
+        {
+            try
+            {
+                var ob = transactionRepository.withdraw(value);
+                if (ob == null)
+                {
+                    return NotFound();
+                }
+                return Ok(ob);
+            }
+            catch (Exception)
+            {
+                return BadRequest();
+            }
+        }
+        [HttpPost]
+        [Route("transfer")]
+        public IActionResult transfer([FromBody] transfers value)
+        {
+            try
+            {
+                var ob = transactionRepository.transfer(value);
+                if (ob == null)
+                {
+                    return NotFound();
+                }
+                return Ok(ob);
+            }
+            catch (Exception)
+            {
+                return BadRequest();
+            }
+        }
+    }
+}
